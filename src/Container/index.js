3e08747755abc3764b0b1f5205f38038e349e9ac
unknown <mateus.saraiva09@gmail.com>
2022-06-08 17:45:31 -0300

    Created a HTML Core and GlobalStyled

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,7 +1,60 @@
 import React from "react";
+import RealImg from "../assets/img/Real.svg"
+import DolarReal from "../assets/img/Dolar.svg"
+import Arrow from "../assets/img/arrowDown.svg"
 
-function App() {
-  return
+function Convert() {
+  return (
+    <div>
+
+      <header>
+        <h1>CodClub Convert Money</h1>
+      </header>
+
+      <main>
+
+        <label>Converter de:
+          <select>
+            <option>R$ Real Brasileiro</option>
+          </select>
+        </label>
+
+        <label>Converter para:
+          <select>
+            <option>US$ Dolar Americano</option>
+            <option>€ Euro</option>
+            <option>₿ BitCoin</option>
+            <option>Ξ Ethereum</option>
+          </select>
+        </label>
+
+        <label>
+          Valor
+          <input placeholder="R$ 10.000,00"></input>
+        </label>
+
+        <button type="button">Converter</button>
+
+        <section>
+
+          <article>
+            <img src={RealImg}></img>
+            <p>Real</p>
+            <p>R$ 0,00</p>
+          </article>
+
+          <img src={Arrow}></img>
+
+          <article>
+            <img src={DolarReal}></img>
+            <p>Dolar</p>
+            <p>$ 0,00</p>
+          </article>
+
+        </section>
+      </main>
+    </div>
+  )
 }
 
-export default App;
+export default Convert;
