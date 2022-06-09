9304982f1c35ae5c70593b149d68956754d2de62
unknown <mateus.saraiva09@gmail.com>
2022-06-09 17:57:28 -0300

    Created first part Styled

--- src/Container/index.js
+++ src/Container/index.js
@@ -2,58 +2,66 @@ import React from "react";
 import RealImg from "../assets/Real.svg"
 import DolarReal from "../assets/Dolar.svg"
 import Arrow from "../assets/arrowDown.svg"
+import { Container, Body, Header, H1, Main, Label, Select, Option, Input, Button } from "./style.js"
 
 function Convert() {
   return (
-    <div>
 
-      <header>
-        <h1>CodClub Convert Money</h1>
-      </header>
+    <Body>
 
-      <main>
+      <Container>
 
-        <label>Converter de:
-          <select>
-            <option>R$ Real Brasileiro</option>
-          </select>
-        </label>
+        <Header>
+          <H1>CodClub Convert Money</H1>
+        </Header>
 
-        <label>Converter para:
-          <select>
-            <option>US$ Dolar Americano</option>
-            <option>€ Euro</option>
-            <option>₿ BitCoin</option>
-            <option>Ξ Ethereum</option>
-          </select>
-        </label>
+        <Main>
 
-        <label>
-          Valor
-          <input placeholder="R$ 10.000,00"></input>
-        </label>
+          <Label>Converter de:
+            <Select>
+              <Option>R$ Real Brasileiro</Option>
+            </Select>
+          </Label>
 
-        <button type="button">Converter</button>
+          <Label>Converter para:
+            <Select>
+              <Option>US$ Dolar Americano</Option>
+              <Option>€ Euro</Option>
+              <Option>₿ BitCoin</Option>
+              <Option>Ξ Ethereum</Option>
+            </Select>
+          </Label>
 
-        <section>
+          <Label>
+            Valor:
+            <Input type="number" placeholder="R$ 10.000,00"></Input>
+          </Label>
 
-          <article>
-            <img src={RealImg}></img>
-            <p>Real</p>
-            <p>R$ 0,00</p>
-          </article>
+          <Button type="button">Converter</Button>
 
-          <img src={Arrow}></img>
+          <section>
 
-          <article>
-            <img src={DolarReal}></img>
-            <p>Dolar</p>
-            <p>$ 0,00</p>
-          </article>
+            <article>
+              <img src={RealImg}></img>
+              <p>Real</p>
+              <p>R$ 0,00</p>
+            </article>
 
-        </section>
-      </main>
-    </div>
+            <img src={Arrow}></img>
+
+            <article>
+              <img src={DolarReal}></img>
+              <p>Dolar</p>
+              <p>$ 0,00</p>
+            </article>
+
+          </section>
+        </Main>
+
+      </Container>
+
+    </Body>
+
   )
 }
 
--- src/Container/style.js
+++ src/Container/style.js
@@ -1,2 +1,102 @@
 import styled from "styled-components";
 
+export const Body = styled.body `
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #E5E5E5;
+`
+
+export const Container = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column nowrap;
+
+`
+
+export const Header = styled.header `
+    background-color: #000000;
+    padding: 1rem 1.5rem;
+
+`
+
+export const H1 = styled.h1 `
+    color: #ffffff;
+    font-size: 2rem;
+    font-weight: 100;
+`
+
+export const Main = styled.main `
+    width: 100%;
+    max-height: 100vh;
+    padding: 1.5rem 1rem;
+    display: flex;
+    background-color: #f4f4f4;
+    flex-flow: column wrap;
+`
+
+export const Label = styled.label `
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    margin-bottom: 1rem;
+    color: #777777;
+    font-weight: 300;
+    font-size: 0.9rem;
+`
+
+export const Select = styled.select `
+    margin-top: 0.3rem;
+    background: #FFFFFF;
+    border: 1px solid #BBBBBB;
+    box-sizing: border-box;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 4px;
+    padding: 1em 0.5em;
+    padding-left: 16px;
+    display: block;
+    outline: none;
+    appearance: none;
+    font-weight: 400;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #F5F9FD;
+    }
+`
+
+export const Option = styled.option `
+    font-weight: 300;
+`
+
+export const Input = styled.input `
+    margin-top: 0.3rem;
+    background: #FFFFFF;
+    border: 1px solid #BBBBBB;
+    box-sizing: border-box;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.15);
+    border-radius: 4px;
+    padding: 1em 0.5em;
+    padding-left: 16px;
+    display: block;
+    outline: none;
+    appearance: none;
+    font-weight: 400;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #F5F9FD;
+    }
+
+
+    &:focus {
+        background-color: #F5F9FD;
+    }
+
+    &[type=number]::-webkit-inner-spin-button { 
+        -webkit-appearance: none;
+    }
+`
